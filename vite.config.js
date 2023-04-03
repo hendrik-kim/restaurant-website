1646e64658feba667d25973ecaf6bb4368fea454
Hendrik Kim <hkim@obsglobal.com>
2023-04-02 22:44:21 -0500

    edit

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/restaurant-website/',
   plugins: [
     vue({
       template: { transformAssetUrls }
